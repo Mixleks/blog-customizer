01a202f73e6ef8145e440587d9aad1658b16d97d
MikhailMozhaev <miselmozaev@gmail.com>
2025-10-31 16:11:14 +0300

    100_Complet

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { Select } from 'src/ui/select/Select';
 import { RadioGroup } from 'src/ui/radio-group/RadioGroup';
-import styles from './ArticleParamsForm.module.scss';
 import { Separator } from 'src/ui/separator/Separator';
 import { Text } from 'src/ui/text/Text';
-import clsx from 'clsx';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import styles from './ArticleParamsForm.module.scss';
 
 import {
 	fontFamilyOptions,
@@ -29,14 +31,23 @@ export const ArticleParamsForm = ({
 	onChange,
 	defaultOpen = false,
 }: Props) => {
-	const [open, setOpen] = useState(defaultOpen);
+	const [open, setOpen] = useState(!defaultOpen);
 	const [values, setValues] = useState<ArticleStateType>(initialValues);
 	useEffect(() => setValues(initialValues), [initialValues]);
+
+	const rootRef = useRef<HTMLDivElement>(null);
+	useOutsideClickClose({
+		isOpen: open,
+		rootRef,
+		onClose: () => setOpen(false),
+		onChange: setOpen,
+	});
 
 	const submit: React.FormEventHandler<HTMLFormElement> = (e) => {
 		e.preventDefault();
 		onChange(values);
 	};
+
 	const reset: React.FormEventHandler<HTMLFormElement> = (e) => {
 		e.preventDefault();
 		setValues(defaultArticleState);
@@ -45,85 +56,75 @@ export const ArticleParamsForm = ({
 
 	return (
 		<>
-			<ArrowButton isOpen={open} onClick={() => setOpen(!open)} />
+			<ArrowButton isOpen={open} onClick={() => setOpen((v) => !v)} />
 
-			<aside className={clsx(styles.container, open && styles.container_open)}>
-				<form className={styles.form} onSubmit={submit} onReset={reset}>
-					<div className={styles.titleWrap}>
-						<Text as='h2' size={31} weight={800}>
-							Задайте параметры
-						</Text>
-					</div>
-					{/* Шрифт */}
-					<fieldset className={styles.fieldset}>
-						<legend className={styles.legend}>Шрифт</legend>
+			<div ref={rootRef}>
+				<aside
+					className={clsx(styles.container, open && styles.container_open)}
+					aria-hidden={!open}>
+					<form className={styles.form} onSubmit={submit} onReset={reset}>
+						<div className={styles.titleWrap}>
+							<Text as='h2' size={31} weight={800}>
+								Задайте параметры
+							</Text>
+						</div>
+
+						{/* Шрифт */}
 						<Select
-							title=''
+							title='Шрифт'
 							options={fontFamilyOptions}
 							selected={values.fontFamilyOption}
+							placeholder='Выберите шрифт'
 							onChange={(o) =>
 								setValues((v) => ({ ...v, fontFamilyOption: o }))
 							}
-							placeholder='Выберите шрифт'
 						/>
-					</fieldset>
 
-					{/*радиогруппа */}
-					<fieldset className={styles.fieldset}>
-						<legend className={styles.legend}>Размер шрифта</legend>
+						{/* Размер шрифта */}
 						<RadioGroup
 							name='font-size'
-							title=''
+							title='Размер шрифта'
 							options={fontSizeOptions}
 							selected={values.fontSizeOption}
 							onChange={(o) => setValues((v) => ({ ...v, fontSizeOption: o }))}
 						/>
-					</fieldset>
 
-					{/* Цвет шрифта */}
-					<fieldset className={styles.fieldset}>
-						<legend className={styles.legend}>Цвет шрифта</legend>
+						{/* Цвет шрифта */}
 						<Select
-							title=''
+							title='Цвет шрифта'
 							options={fontColors}
 							selected={values.fontColor}
-							onChange={(o) => setValues((v) => ({ ...v, fontColor: o }))}
 							placeholder='Выберите цвет'
+							onChange={(o) => setValues((v) => ({ ...v, fontColor: o }))}
 						/>
-					</fieldset>
-					<fieldset className={styles.fieldset}>
-						<Separator></Separator>
-					</fieldset>
-					{/* Цвет фона */}
-					<fieldset className={styles.fieldset}>
-						<legend className={styles.legend}>Цвет фона</legend>
+
+						<Separator />
+
+						{/* Цвет фона */}
 						<Select
-							title=''
+							title='Цвет фона'
 							options={backgroundColors}
 							selected={values.backgroundColor}
-							onChange={(o) => setValues((v) => ({ ...v, backgroundColor: o }))}
 							placeholder='Выберите цвет'
+							onChange={(o) => setValues((v) => ({ ...v, backgroundColor: o }))}
 						/>
-					</fieldset>
 
-					{/* Ширина контента*/}
-					<fieldset className={styles.fieldset}>
-						<legend className={styles.legend}>Ширина контента</legend>
+						{/* Ширина контента */}
 						<Select
-							title=''
+							title='Ширина контента'
 							options={contentWidthArr}
 							selected={values.contentWidth}
-							onChange={(o) => setValues((v) => ({ ...v, contentWidth: o }))}
 							placeholder='Выберите ширину'
+							onChange={(o) => setValues((v) => ({ ...v, contentWidth: o }))}
 						/>
-					</fieldset>
 
-					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
-						<Button title='Применить' htmlType='submit' type='apply' />
-					</div>
-				</form>
-			</aside>
+						<div className={styles.bottomContainer}>
+							<Button title='Сбросить' htmlType='reset' type='clear' />
+							<Button title='Применить' htmlType='submit' type='apply' />
+						</div>
+					</form>
+				</aside>
+			</div>
 		</>
 	);
 };
